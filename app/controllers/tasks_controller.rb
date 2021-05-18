e4985cc172ee0eb43e22f5a9b3cda2fc5cc2e55b
Francesca Fenu <fenufrancesca@yahoo.de>
2021-05-18 14:30:58 +0200

    refactoring

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,12 @@
 class TasksController < ApplicationController
-  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
-  
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
   end
 
   def new
@@ -21,18 +21,18 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
     @task.update(task_params)
 
     redirect_to task_path(@task)
   end
 
   def destroy
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
     @task.destroy
 
     redirect_to tasks_path
